f7283ab57a9174af5d19663d41ee9545081cba89
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2021-01-05 23:20:22 -0700

    wip

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -27,19 +27,19 @@ export default function Post() {
 //   </Layout>)
 // }
 
-// export async function getStaticPaths() {
-//   const paths = getAllPostIds()
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
-// export async function getStaticProps({ params }) {
-//   const postData = await getPostData(params.id)
-//   return {
-//     props: {
-//       postData
-//     }
-//   }
-// }
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
